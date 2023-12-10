bedc6841d36d0ddf2fbdc2f3f7a5199cc3ce77a9
Djounna <80339248+Djounna@users.noreply.github.com>
2023-12-10 12:35:35 +0100

    #Persistence.DbContext

---
+++ CM.Backend/CM.Backend.Persistence/SQL/CMContext.cs
@@ -0,0 +1,32 @@
+﻿using CM.Backend.Domain.CowDetails;
+using CM.Backend.Domain.Cows;
+using CM.Backend.Domain.Externals;
+using CM.Backend.Domain.Infrastructures;
+using CM.Backend.Domain.Jobs;
+using CM.Backend.Domain.Workers;
+using Microsoft.EntityFrameworkCore;
+
+namespace CM.Backend.Persistence.EF;
+
+public class CMContext : DbContext
+{
+    public CMContext(DbContextOptions<CMContext> options) : base(options) { }
+
+    public DbSet<Cow> Cows { get; set; }
+    public DbSet<Group> Groups { get; set; }
+
+    public DbSet<Pen> Pens { get; set; }
+    public DbSet<Stock> Stocks { get; set; }
+
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<RecurringJob> RecurringJobs { get; set; }
+    public DbSet<WorkerJob> WorkerJobs { get; set; }
+
+    public DbSet<Worker> Workers { get; set; }
+    public DbSet<Intervenant> Intervenants { get; set; }
+
+    public DbSet<Gestation> Gestations { get; set; }
+    public DbSet<Intervention> Interventions { get; set; }
+    public DbSet<MilkProduction> MilkProductions { get; set; }
+    public DbSet<Milking> Milkings { get; set; }
+}
