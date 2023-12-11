01a55c2a1275aeaa6a095c477f45983efbbb3464
Djounna <80339248+Djounna@users.noreply.github.com>
2023-12-12 00:03:02 +0100

    #WIP

--- CM.Backend/CM.Backend.Application/Interfaces/Persistence/IBaseRepository.cs
+++ CM.Backend/CM.Backend.Application/Interfaces/Persistence/IBaseRepository.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 namespace CM.Backend.Application.Interfaces.Persistence;
 public interface IBaseRepository<T> where T : class 
 {
-    Task<T> GetById(int id);
-    Task<IReadOnlyList<T>> GetList();
-    Task<T> Create(T entity);
-    Task<T> Update(T entity);
-    Task Delete(int id);
+    Task<T> GetById(int id, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<T>> GetList(CancellationToken cancellationToken = default);
+    Task<T> Create(T entity, CancellationToken cancellationToken = default);
+    Task<T> Update(T entity, CancellationToken cancellationToken = default);
+    Task Delete(int id, CancellationToken cancellationToken = default);
 }
---
+++ CM.Backend/CM.Backend.Application/Services/Cow/Queries/GetCowByIdQuery.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CM.Backend.Application.Interfaces.Persistence;
+using CM.Backend.Application.Models.Cows;
+using MediatR;
+
+namespace CM.Backend.Application.Services.Cow.Queries;
+
+public record GetCowByIdQuery(int id) : IRequest<CowDto>;
+
+public class GetCowByIdQueryHandler : IRequestHandler<GetCowByIdQuery, CowDto>
+{
+    private readonly ICowRepository _cowRepository;
+    private readonly IMapper _mapper;
+
+    public GetCowByIdQueryHandler(ICowRepository cowRepository, IMapper mapper)
+    {
+        _cowRepository = cowRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<CowDto> Handle(GetCowByIdQuery request, CancellationToken cancellationToken)
+    {
+       return await _cowRepository.GetById(request.id, cancellationToken);
+    }
+}
