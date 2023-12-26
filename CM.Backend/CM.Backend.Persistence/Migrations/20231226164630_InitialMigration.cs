dfe9cb48ec214ccf7c33628f84515d0e4ac06ad1
Djounna <80339248+Djounna@users.noreply.github.com>
2023-12-26 18:47:22 +0100

    #Initial_DBMigration

---
+++ CM.Backend/CM.Backend.Persistence/Migrations/20231226164630_InitialMigration.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CM.Backend.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Intervenants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Coordinates = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Intervenants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Area = table.Column<int>(type: "int", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecurringJobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Length = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecurringJobs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stocks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cows",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Identifier = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Race = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sex = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PenId = table.Column<int>(type: "int", nullable: true),
+                    GroupId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cows", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cows_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Cows_Pens_PenId",
+                        column: x => x.PenId,
+                        principalTable: "Pens",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alerts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CowId = table.Column<int>(type: "int", nullable: true),
+                    PenId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alerts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alerts_Cows_CowId",
+                        column: x => x.CowId,
+                        principalTable: "Cows",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Alerts_Pens_PenId",
+                        column: x => x.PenId,
+                        principalTable: "Pens",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gestations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CowId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    CalvingDate = table.Column<DateOnly>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gestations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Gestations_Cows_CowId",
+                        column: x => x.CowId,
+                        principalTable: "Cows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interventions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CowId = table.Column<int>(type: "int", nullable: false),
+                    IntervenantId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interventions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Interventions_Cows_CowId",
+                        column: x => x.CowId,
+                        principalTable: "Cows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Interventions_Intervenants_IntervenantId",
+                        column: x => x.IntervenantId,
+                        principalTable: "Intervenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Length = table.Column<int>(type: "int", nullable: false),
+                    RecurringJobId = table.Column<int>(type: "int", nullable: true),
+                    PenId = table.Column<int>(type: "int", nullable: true),
+                    CowId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jobs_Cows_CowId",
+                        column: x => x.CowId,
+                        principalTable: "Cows",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Jobs_Pens_PenId",
+                        column: x => x.PenId,
+                        principalTable: "Pens",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Jobs_RecurringJobs_RecurringJobId",
+                        column: x => x.RecurringJobId,
+                        principalTable: "RecurringJobs",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MilkProductions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CowId = table.Column<int>(type: "int", nullable: false),
+                    WeeklyProduction = table.Column<double>(type: "float", nullable: false),
+                    MonthlyProduction = table.Column<double>(type: "float", nullable: false),
+                    LifetimeAverage = table.Column<double>(type: "float", nullable: false),
+                    TotalQuantity = table.Column<double>(type: "float", nullable: false),
+                    TotalDays = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MilkProductions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MilkProductions_Cows_CowId",
+                        column: x => x.CowId,
+                        principalTable: "Cows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkerJobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WorkerId = table.Column<int>(type: "int", nullable: false),
+                    JobId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkerJobs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkerJobs_Jobs_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Jobs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkerJobs_Workers_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "Workers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Milkings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MilkProductionId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Volume = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Milkings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Milkings_MilkProductions_MilkProductionId",
+                        column: x => x.MilkProductionId,
+                        principalTable: "MilkProductions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_CowId",
+                table: "Alerts",
+                column: "CowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_PenId",
+                table: "Alerts",
+                column: "PenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cows_GroupId",
+                table: "Cows",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cows_PenId",
+                table: "Cows",
+                column: "PenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gestations_CowId",
+                table: "Gestations",
+                column: "CowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interventions_CowId",
+                table: "Interventions",
+                column: "CowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interventions_IntervenantId",
+                table: "Interventions",
+                column: "IntervenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_CowId",
+                table: "Jobs",
+                column: "CowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_PenId",
+                table: "Jobs",
+                column: "PenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_RecurringJobId",
+                table: "Jobs",
+                column: "RecurringJobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Milkings_MilkProductionId",
+                table: "Milkings",
+                column: "MilkProductionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MilkProductions_CowId",
+                table: "MilkProductions",
+                column: "CowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkerJobs_JobId",
+                table: "WorkerJobs",
+                column: "JobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkerJobs_WorkerId",
+                table: "WorkerJobs",
+                column: "WorkerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alerts");
+
+            migrationBuilder.DropTable(
+                name: "Gestations");
+
+            migrationBuilder.DropTable(
+                name: "Interventions");
+
+            migrationBuilder.DropTable(
+                name: "Milkings");
+
+            migrationBuilder.DropTable(
+                name: "Stocks");
+
+            migrationBuilder.DropTable(
+                name: "WorkerJobs");
+
+            migrationBuilder.DropTable(
+                name: "Intervenants");
+
+            migrationBuilder.DropTable(
+                name: "MilkProductions");
+
+            migrationBuilder.DropTable(
+                name: "Jobs");
+
+            migrationBuilder.DropTable(
+                name: "Workers");
+
+            migrationBuilder.DropTable(
+                name: "Cows");
+
+            migrationBuilder.DropTable(
+                name: "RecurringJobs");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Pens");
+        }
+    }
+}
