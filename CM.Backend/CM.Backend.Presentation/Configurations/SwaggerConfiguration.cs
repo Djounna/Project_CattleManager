9aef1582af1e1dad71bda109bcd59a88533615c1
Djounna <80339248+Djounna@users.noreply.github.com>
2023-12-27 14:00:31 +0100

    #Authorization Add SwaggerGen Configuration

---
+++ CM.Backend/CM.Backend.Presentation/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace CM.Backend.Presentation.Configurations;
+
+public static class SwaggerConfiguration
+{
+   public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(opt =>
+        {
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please provide the token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+        });
+
+        return services;
+    }
+}
