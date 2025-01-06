40f92fb2858005431be5721844648e2a7b87d56f
Djounna <80339248+Djounna@users.noreply.github.com>
2025-01-06 21:04:01 +0100

    PicklistTool - Missing Dto

---
+++ CM.Portal/src/app/api/models/assign-pen-details-dto.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+import { PenDetailsDto } from './pen-details-dto';
+export interface AssignPenDetailsDto {
+  pen1?: PenDetailsDto;
+  pen2?: PenDetailsDto;
+}
