e652b149bd5d94e0f0f3172c678569438c5e2de9
Djounna <80339248+Djounna@users.noreply.github.com>
2025-02-02 15:54:27 +0100

    Code cleaning

--- CM.Portal/src/app/screens/authentication/login-page/login-page.component.ts
+++ CM.Portal/src/app/screens/authentication/login-page/login-page.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-page',
   standalone: false,
-  imports: [],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
