bee4e66e8499f836bf4f9b6558a76c1c33085e90
Djounna <80339248+Djounna@users.noreply.github.com>
2025-03-17 19:49:29 +0100

    FLUTTER - OpenApi Generator Config

---
+++ CM_App/lib/api/api_gen.dart
@@ -0,0 +1,23 @@
+// Openapi Generator last run: : 2025-03-17T19:42:39.907886
+
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+/*@Openapi(
+  additionalProperties:
+    AdditionalProperties(pubName: 'CM_api', pubAuthor: 'Corentin', useEnumExtension: true),
+    inputSpec:
+    RemoteSpec(path: 'https://localhost: /v1/swagger.json'),
+    generatorName: Generator.dart,
+    runSourceGenOnOutput: true,
+    outputDirectory: 'CM_api')
+class OtherApiGenerator {}*/
+
+ @Openapi(
+  additionalProperties:
+  DioProperties(pubName: 'CM_api', pubAuthor: 'Corentin', useEnumExtension: true),
+  inputSpec: InputSpec(path: 'lib/api/swagger.yaml'),
+  generatorName: Generator.dio,
+  runSourceGenOnOutput: true,
+  outputDirectory: 'CM-Web-api',
+)
+class OpenApiGenerator {}
