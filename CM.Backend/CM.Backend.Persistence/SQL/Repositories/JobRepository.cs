ea82e449a85cd8c77ac7e0f09b68d129f32e395d
Djounna <corentin.deconninck@hotmail.com>
2025-06-17 16:49:23 +0200

    WorkerDashboard - Added GetJobsByUserByDate logic

--- CM.Backend/CM.Backend.Persistence/SQL/Repositories/JobRepository.cs
+++ CM.Backend/CM.Backend.Persistence/SQL/Repositories/JobRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CM.Backend.Application.Interfaces.Persistence;
 using CM.Backend.Domain.Jobs;
+using CM.Backend.Domain.Users;
 using CM.Backend.Persistence.EF;
 
 namespace CM.Backend.Persistence.SQL.Repositories;
@@ -10,5 +11,10 @@ public class JobRepository : BaseRepository<Job>, IJobRepository
     public IEnumerable<Job> GetListByDate(DateOnly date)
     {
         return _context.Jobs.Where(m => m.Date == date).ToList();
+    }
+
+    public IEnumerable<Job> GetListByUserByDate(int userId, DateOnly date)
+    {
+        return _context.Jobs.Where(j => j.Date == date && j.WorkerJobs.Any(wj => wj.UserId == userId));
     }
 }
